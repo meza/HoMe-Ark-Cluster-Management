9d9173c07a78e68c9061af4c521ba72fea0bad6a
meza <meza@users.noreply.github.com>
2023-05-30 12:33:24 +0100

    chore: added event announcement logic

--- gsa-init.sh
+++ gsa-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+WEBHOOK_URL="https://discord.com/api/webhooks/941301089305767986/6LCTr29ms0edoQBepOMj_hIGiSepp_IgYsPGf3sc_hojjImUxCNHoW64LfqckvnVmxug"
 BASEDIR=${1:-/serverfiles}
 EVENTS_FILE=/tmp/events.csv
 PREFIX="\[HoMe\]"
@@ -12,17 +12,49 @@ if [ ! -f $GSA_CONTROL ]; then
   exit 1;
 fi
 
-echo "Fetching the list of events"
-curl -s -o /tmp/events.csv -f https://raw.githubusercontent.com/meza/HoMe-Ark-Cluster-Management/main/events.csv
 
-# Set active event
+# -------------------------------------------- Functions ---------------------------------------------------------------
 
-#Parse the events.csv file and return the active event string for today
-#The events.csv file should be in the following format:
-#start_year;start_month;start_day;end_year;end_month;end_day;event_name
-#all dates are inclusive
-#If no event is active, return an empty string
+get_long_name() {
+  local short_name="{$1,,}"
+
+  case $short_name in
+    vday)
+      echo "Love Evolved"
+      ;;
+    easter)
+      echo "Eggcelect Adventure"
+      ;;
+    birthday)
+      echo "Anniversary"
+      ;;
+    summer)
+      echo "Summer Bash"
+      ;;
+    fearevolved)
+      echo "Fear Evolved"
+      ;;
+    turkeytrial)
+      echo "Turkey Trial"
+      ;;
+    winterwonderland)
+      echo "Winter Wonderland"
+      ;;
+    *)
+      echo ""
+      ;;
+  esac
+}
+
 get_active_event() {
+  # Parse the events.csv file and return the active event string for today
+  # The events.csv file should be in the following format:
+  # start_year;start_month;start_day;end_year;end_month;end_day;event_name
+  # all dates are inclusive
+
+  echo "Fetching the list of events"
+  curl -s -o $EVENTS_FILE -f https://raw.githubusercontent.com/meza/HoMe-Ark-Cluster-Management/main/events.csv
+
   local date=$(date +%s)
   local start_year start_month start_day end_year end_month end_day event_name
   while IFS=';' read -r start_year start_month start_day end_year end_month end_day event_name; do
@@ -36,6 +68,8 @@ get_active_event() {
   echo ""
 }
 
+# ----------------------------------------- SET ACTIVE EVENT -----------------------------------------------------------
+
 ACTIVE_EVENT="$(get_active_event)"
 
 if [ -n "$ACTIVE_EVENT" ]; then
@@ -45,18 +79,38 @@ else
 fi
 
 ## Clear the flag
-sed -i "s/%%EVENT%%/-ActiveEvent=$ACTIVE_EVENT/g" $GSA_CONTROL
+sed -i "s/%%EVENT%%/-ActiveEvent=$ACTIVE_EVENT/g" "$GSA_CONTROL"
+
+if [ ! -f "${BASEDIR}/currentevent.txt" ] && [ -n "$ACTIVE_EVENT" ]; then
+  echo "$ACTIVE_EVENT" > "${BASEDIR}/currentevent.txt"
+  echo "The event $ACTIVE_EVENT is active"
+  if grep -q "$PREFIX Island" "$GSA_CONTROL"; then
+    echo "Broadcasting the event"
+  fi
+fi
+
+if [ -f "${BASEDIR}/currentevent.txt" ] && [ -z "$ACTIVE_EVENT" ]; then
+  CURRENT_EVENT=$(cat "${BASEDIR}/currentevent.txt")
+  rm "${BASEDIR}/currentevent.txt"
+  echo "The event $CURRENT_EVENT is no longer active"
+  if grep -q "$PREFIX Island" "$GSA_CONTROL"; then
+      echo "Broadcasting the end of the event"
+  fi
+fi
+
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 # Override Settings
 
 ## Aberration
-if grep -q "$PREFIX Aberration" $GSA_CONTROL; then
+if grep -q "$PREFIX Aberration" "$GSA_CONTROL"; then
   echo "Overriding the DayTimeScale to be default"
   sed -i "s/DayTimeSpeedScale=.5/DayTimeSpeedScale=1/g" $GUS
 fi
 
 ## Overrides Non-Story maps
-if grep -q "No Item / Dino Transfer" $GSA_CONTROL; then
+if grep -q "No Item / Dino Transfer" "$GSA_CONTROL"; then
   echo "Overriding Transfer Prevention"
   sed -i "s/PreventUploadItems=False/PreventUploadItems=True/g" $GUS
   sed -i "s/PreventUploadDinos=False/PreventUploadDinos=True/g" $GUS
